ce87c2926bd78234591ad0b104dae3eaefbcb373
Tomasz Kalfas <tomasz.kalfas@first-utility.com>
2020-08-25 10:36:14 +0200

    Tests updated post SSR

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import {
+  BrowserRouter as Router
+} from "react-router-dom";
 import App from './App';
 
 describe('App component', () => {
   it('renders as expected', () => {
     const app = mount(
-      <App/>
+      <Router>
+        <App/>
+      </Router>
     );
     expect(app).toMatchSnapshot();
   });
